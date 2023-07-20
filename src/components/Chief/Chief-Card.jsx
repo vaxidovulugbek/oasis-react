4f51c448f66ead2eaa627f32321d6f26170ff72b
vakhidovulugbek03 <vulugbekk@gmail.com>
2023-07-20 15:35:05 +0500

    commit

--- src/components/Chief/Chief-Card.jsx
+++ src/components/Chief/Chief-Card.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import image30_1 from '../../assets/imgs/png/image 30 (1).png'
-import image30_2 from '../../assets/imgs/png/image 32.png'
 import image30_3 from '../../assets/imgs/png/Images.png'
-import frame from '../../assets/imgs/png/Frame 168.png'
 function Chief_Card() {
   const data = [
     {
@@ -29,13 +26,6 @@ function Chief_Card() {
   return (
     <>
       <section className="info-aquapark-content">
-        <div className="info-aquapark">
-          <h1 className="info-aquapark__title">«Кухня»</h1>
-          <p className="info-aquapark__text">В аквапарке очень хорошая кухня, шеф-повар каждый день выходит в кухню и проверяет качество еды и даже сам садится и обедает, а это не часто увидишь в парках. Есть рай блюдо на любой вкус и холодные напитки.</p>
-          <img className="info-aquapark__wave-img" src={frame} alt />
-        </div>
-        <img className="info-aquapark__img" src={image30_1} alt />
-        <img className="info-aquapark__subimg" src={image30_2} alt />
         <ul className="d-flex aquapark-chef-list">
 
           {
