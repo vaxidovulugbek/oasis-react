c10b27341a2eab8459d48f1617fa222859b852d9
vakhidovulugbek03 <vulugbekk@gmail.com>
2023-07-25 12:26:45 +0500

    commit

--- src/components/Event_Cards/Event_Cards.jsx
+++ src/components/Event_Cards/Event_Cards.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import image_card from '../../assets/imgs/png/Снимок экрана 2023-06-28 в 01.01 1.png'
 import arrow from '../../assets/imgs/svg/arrow_back_ios_new.svg'
 import {v4 as uuidv4} from 'uuid';
+import InputMask from 'react-input-mask';
+
 function Event_Cards() {
+
+  const [phone, setPhone] = useState("");
+  const handleInput = ({ target: { value } }) => setPhone(value);
+
   const data = [
     {
       img:image_card,
@@ -57,6 +63,7 @@ function Event_Cards() {
                 <p className="info-aquapark__event-date">{el?.date}</p>
                 <div className="d-flex align-items-center justify-content-center gap-3">
                     <button data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" className="info-aquapark__bron">Бронировать</button>
+                    <button data-bs-target="#exampleModalToggle" data-bs-toggle="modal" className="info-aquapark__more">Подробнее <img src={arrow} alt="" /></button>
                 </div>
               </div>
             </li>
@@ -66,34 +73,61 @@ function Event_Cards() {
         </ul>
       </section>
 
-
+{/* START OF MODALS */}
     <div className="modall-bron">
-    <div className="modal fade" id="exampleModalToggle2" aria-labelledby="exampleModalToggleLabel" tabIndex={-1} aria-hidden="true" style={{display: 'none'}}>
-      <div className="modal-dialog modal-dialog-centered">
-        <div className="modal-content">
-          <div className="modal-body modal-bron-body">
-            <img className="modall-bron__img" src="https://picsum.photos/id/48/618/584" alt="" />
-            <div className="d-flex flex-column modall-bron__content">
-              <h2 className="modall-bron__title">«Пираты Карибского моря»</h2>
-              <div className="mb-2">
-                <p className="modall-bron__subtext">В программу входит:</p>
-                <p className="modall-bron__text">Часовая анимационная программа
-                  (Программа включает 4 аниматора: 3 актёра и 1 аква-няня)</p>
-              </div>
-              <div>
-                <p className="modall-bron__subtext">3 часа в аквазоне:</p>
-                <p className="modall-bron__text">- Бумажное шоу</p>
-                <p className="modall-bron__text">- Праздничный банкет</p>
-                <p className="modall-bron__text">- Отдых в аквазоне/детской игровой зоне</p>
-                <p className="modall-bron__text">- Шарики- Фотосессия</p>
+      <div className="modal fade" id="exampleModalToggle2" aria-labelledby="exampleModalToggleLabel" tabIndex={-1} aria-hidden="true" style={{display: 'none'}}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-body modal-bron-body">
+              <img className="modall-bron__img" src="https://picsum.photos/id/48/618/584" alt="" />
+              <div className="d-flex flex-column modall-bron__content">
+                <h2 className="modall-bron__title">«Пираты Карибского моря»</h2>
+                <div className="mb-2">
+                  <p className="modall-bron__subtext">В программу входит:</p>
+                  <p className="modall-bron__text">Часовая анимационная программа
+                    (Программа включает 4 аниматора: 3 актёра и 1 аква-няня)</p>
+                </div>
+                <div>
+                  <p className="modall-bron__subtext">3 часа в аквазоне:</p>
+                  <p className="modall-bron__text">- Бумажное шоу</p>
+                  <p className="modall-bron__text">- Праздничный банкет</p>
+                  <p className="modall-bron__text">- Отдых в аквазоне/детской игровой зоне</p>
+                  <p className="modall-bron__text">- Шарики- Фотосессия</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
+    <div className="modall-more">
+      <div className="modal fade" id="exampleModalToggle" aria-labelledby="exampleModalToggleLabel" tabIndex={-1} aria-hidden="true" style={{display: 'none'}}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <form className="modal-body">
+              <p className="aquapark-event-title">Оставьте заявку на бронирование</p>
+              <p className="aquapark-event-text">мы с ввяжемся с вами в ближайшее время</p>
+              <fieldset className="d-flex flex-column">
+                <input className="aquapark-event-input mb-2" type="text" placeholder="Ваша имя" />
+                <div className='aquapark-event-input'>
+                  <span>+998 </span>
+                  <InputMask 
+                    placeholder='(__)___ __ __'
+                    mask='(99) 999 99 99' 
+                    value={phone} 
+                    onChange={handleInput}>
+                  </InputMask>
+                </div>
+              </fieldset>
+              <button className="aquapark-event-btn" type="submit">Забронировать</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+{/* END OF MODALS */}
     </>
   )
 }
